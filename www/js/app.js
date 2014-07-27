8088c281fccfe1106f4df8467f2089df225d8e9f
Kola Ashiru <kashiru@gmail.com>
2014-07-27 21:52:58 +0100

    re-baseline

--- www/js/app.js
+++ www/js/app.js
@@ -4,10 +4,15 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 // 'starter.controllers' is found in controllers.js
-var kwikii_app = angular.module('starter', ['ionic', 'starter.controllers'])
+angular.module('starter', ['ionic', 'starter.controllers'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
     if(window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
@@ -42,7 +47,6 @@ var kwikii_app = angular.module('starter', ['ionic', 'starter.controllers'])
         }
       }
     })
-
     .state('app.playlists', {
       url: "/playlists",
       views: {
@@ -61,127 +65,8 @@ var kwikii_app = angular.module('starter', ['ionic', 'starter.controllers'])
           controller: 'PlaylistCtrl'
         }
       }
-    })
-      .state('app.sign_up', {
-          url: "/sign_up",
-          views: {
-              'menuContent' :{
-                  templateUrl: "templates/sign_up.html"
-              }
-          }
-      })
-      .state('app.sign_in', {
-          url: "/sign_in",
-          views: {
-              'menuContent' :{
-                  templateUrl: "templates/sign_in.html"
-              }
-          }
-      })
-      .state('app.home', {
-          url: "/home",
-          views: {
-              'menuContent' :{
-                  templateUrl: "templates/home.html"
-              }
-          }
-      })
-      .state('app.find_merchant', {
-          url: "/find_merchant",
-          views: {
-              'menuContent' :{
-                  templateUrl: "templates/find_merchant.html"
-              }
-          }
-      })
-      .state('app.find_contact', {
-          url: "/find_contact",
-          views: {
-              'menuContent' :{
-                  templateUrl: "templates/find_contact.html",
-                  controller: "ContactCtrl"
-              }
-          },
-          resolve: {
-              contacts: function(LocalContactService) {
-                  return LocalContactService.getContacts()
-              }
-          }
-      })
-      .state('app.payment_confirmation', {
-          url: "/payment_confirmation/:contact_id",
-          views: {
-              'menuContent' :{
-                  templateUrl: "templates/payment_confirmation.html",
-                  controller: "ConfirmationCtrl"
-              }
-          },
-          resolve: {
-              contact: function($stateParams, LocalContactService) {
-                  return LocalContactService.getContact($stateParams.contact_id)
-              }
-          }
-      })
-      .state('app.transfer', {
-          url: "/transfer/:contact_id",
-          views: {
-              'menuContent' :{
-                  templateUrl: "templates/transfer.html",
-                  controller: "TransferCtrl"
-              }
-          },
-          resolve: {
-              contact: function($stateParams, LocalContactService) {
-                  return LocalContactService.getContact($stateParams.contact_id)
-              }
-          }
-      })
-      .state('app.transactions', {
-          url: "/transactions",
-          views: {
-              'menuContent' :{
-                  templateUrl: "templates/transactions.html",
-                  controller: "TransactionsCtrl"
-              }
-          },
-          resolve: {
-              transactions: function(TransactionService) {
-                  return TransactionService.getTransactions()
-              }
-          }
-      })
-      .state('app.transaction', {
-          url: "/transactions/:transaction_id",
-          views: {
-              'menuContent' :{
-                  templateUrl: "templates/transaction.html",
-                  controller: "TransactionCtrl"
-              }
-          },
-          resolve: {
-              transaction: function($stateParams, TransactionService) {
-                  return TransactionService.getTransaction($stateParams.transaction_id)
-              }
-          }
-      })
-      .state('app.payment_options', {
-          url: "/payment_options",
-          views: {
-              'menuContent' :{
-                  templateUrl: "templates/payment_options.html"
-              }
-          }
-      })
-      .state('app.splash', {
-          url: "/splash",
-          views: {
-              'menuContent' :{
-                  templateUrl: "templates/splash.html",
-                  controller: 'SplashCtrl'
-              }
-          }
-      });
+    });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/splash');
+  $urlRouterProvider.otherwise('/app/playlists');
 });
 
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,72 +1,36 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $location, $ionicActionSheet, $timeout, ContactService) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+  // Form data for the login modal
+  $scope.loginData = {};
 
-        $scope.amount_to_pay = 12000.0;
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
 
-        var rander = Math.random();
-        $ionicModal.fromTemplateUrl('templates/confirm_merchant.html?q=' + rander, {
-            scope: $scope,
-            animation: 'slide-in-up'
-        }).then(function(modal) {
-                $scope.modal = modal;
-            });
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  },
 
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
 
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
 
-
-        $scope.openModal = function() {
-            $scope.modal.show();
-        };
-        $scope.closeModal = function() {
-            $scope.modal.hide();
-        };
-        //Cleanup the modal when we're done with it!
-        $scope.$on('$destroy', function() {
-            $scope.modal.remove();
-        });
-
-
-        $scope.confirmMerchant = function(val){
-          $scope.closeModal();
-          $location.path('/app/payment_confirmation');
-          $scope.confirmed_amount = val;
-        };
-
-        $scope.showPaymentSheet = function() {
-
-            // Show the action sheet
-            $ionicActionSheet.show({
-                buttons: [
-                    { text: 'GTB current account' },
-                    { text: 'Stanbic current account' },
-                    { text: 'New payment option' }
-                ],
-                //destructiveText: '',
-                titleText: 'Select payment method',
-                cancelText: 'Cancel',
-                buttonClicked: function(index) {
-                    return true;
-                }
-            });
-
-        };
-
-
-
-
-        $scope.doRefresh = function() {
-
-            $timeout(function() {
-                $scope.$broadcast('scroll.refreshComplete');
-            }, 1000);
-        };
-
-
-
-
-
-
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
 })
 
 .controller('PlaylistsCtrl', function($scope) {
@@ -80,165 +44,5 @@ angular.module('starter.controllers', [])
   ];
 })
 
-.controller('SplashCtrl', function($scope) {
-        $scope.mode = 'signup';
-
-        $scope.switchMode = function(val){
-            $scope.mode = val;
-        }
-})
-
-    .controller('ContactCtrl', function($scope, ContactService, $ionicActionSheet, $location, contacts) {
-
-
-        $scope.local_contacts = contacts;
-        $scope.state = "find";
-
-        $scope.contacts = [];
-
-
-
-        $scope.findContact = function (contactSearch) {
-            ContactService.find(contactSearch).then(function (contacts) {
-                $scope.contacts = contacts;
-
-            }, function (error) {
-                console.log(error);
-            });
-        };
-
-
-        if(!!window.cordova){
-            $scope.findContact();
-        }
-
-
-        $scope.selectContact = function(contact) {
-
-
-            if(contact.phoneNumbers.length == 0){
-
-            }
-            else if(contact.phoneNumbers.length == 1){
-
-
-                $scope.selected_contact = {name: contact.name.formatted, number: contact.phoneNumbers[0].value}
-                $scope.confirmContact();
-            }else if(contact.phoneNumbers.length > 1){
-
-                var numbers = [];
-
-                for (var j = 0; j < contact.phoneNumbers.length; j++){
-
-                    var entry = {text: contact.phoneNumbers[j].value}
-                    numbers.push(entry);
-
-                }
-
-                // Show the action sheet
-                $ionicActionSheet.show({
-                    buttons: numbers,
-                    //destructiveText: '',
-                    titleText: contact.name.formatted,
-                    cancelText: 'Cancel',
-                    buttonClicked: function(index) {
-
-                        $scope.selected_contact = {name: contact.name.formatted, number: contact.phoneNumbers[index].value};
-                        $scope.confirmContact();
-
-                        return true;
-                    }
-                });
-            }
-        };
-
-        $scope.confirmContact = function(){
-
-
-            //$state.go('app.transfer');
-            $location.path('app/transfer');
-            //$scope.closeContactsModal();
-
-        }
-
-    })
-    .controller('TransferCtrl', function($scope, contact, $ionicPopup, $location) {
-
-        $scope.contact = contact;
-
-        // Triggered on a button click, or some other target
-        $scope.showPopup = function() {
-            $scope.data = {}
-
-            // An elaborate, custom popup
-            var myPopup = $ionicPopup.show({
-                template: '<input type="password" ng-model="data.wifi">',
-                title: 'Enter your kwikii password',
-                subTitle: 'This is a security precaution',
-                scope: $scope,
-                buttons: [
-                    { text: 'Cancel' },
-                    {
-                        text: '<b>Continue</b>',
-                        type: 'button-positive',
-                        onTap: function(e) {
-                            if (!$scope.data.wifi) {
-                                //don't allow the user to close unless he enters wifi password
-                                e.preventDefault();
-                            } else {
-                                //$location.path('app/payment_confirmation')
-                                return $scope.data.wifi;
-                            }
-                        }
-                    },
-                ]
-            });
-            myPopup.then(function(res) {
-                $scope.confirmMerchant(13000);
-                console.log('Tapped!', res);
-            });
-        };
-
-        $scope.confirmMerchant = function(val){
-            //$scope.closeModal();
-            $location.path('/app/payment_confirmation/' + contact.id);
-            $scope.confirmed_amount = val;
-        };
-
-
-
-
-
-    })
-    .controller('TransactionCtrl', function($scope, transaction, $ionicPopup, $location) {
-
-        $scope.transaction = transaction;
-
-
-
-
-
-    })
-    .controller('TransactionsCtrl', function($scope, transactions, $location) {
-
-        $scope.transactions = transactions;
-
-
-
-
-
-    })
-    .controller('ConfirmationCtrl', function($scope, contact, $location) {
-
-        $scope.contact = contact;
-        $scope.confirmed_amount = 12000;
-
-
-
-
-
-    })
-
 .controller('PlaylistCtrl', function($scope, $stateParams) {
-
 })
